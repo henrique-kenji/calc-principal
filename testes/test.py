5db7b74f67e8028f820dd00b7e503b5ab4ad7fc5
henrique-kenji <95830239+henrique-kenji@users.noreply.github.com>
2021-12-09 00:26:53 -0300

    Update test.py

--- testes/test.py
+++ testes/test.py
@@ -1,36 +1,46 @@
 import os
 import sys
 
-scriptpath = "../services/"
-sys.path.append(os.path.abspath(scriptpath))
-from operationsService import *
+# scriptpath = "../services/"
+# sys.path.append(os.path.abspath(scriptpath))
+# from operationsService import *
+import importlib.util
+ 
+def module_from_file(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+ 
+op = module_from_file("elementarFunction", "../services/operationsService.py")
+
 
 import unittest
 
 class OperationTest(unittest.TestCase):
     def testSum(self):
-        resp = elementarFunction (5, 5, '+')
+        resp = op.elementarFunction (5, 5, '+')
         print (resp)
         self.assertEqual(resp[0], 10)
 
     def testSub(self):
-        resp = elementarFunction (5, 5, '-')
+        resp = op.elementarFunction (5, 5, '-')
         self.assertEqual(resp[0], 0)
 
     def testMult(self):
-        resp = elementarFunction (5, 5, '*')
+        resp = op.elementarFunction (5, 5, '*')
         self.assertEqual(resp[0], 25)
 
     def testDiv(self):
-        resp = elementarFunction (5, 5, '/')
+        resp = op.elementarFunction (5, 5, '/')
         self.assertEqual(resp[0], 1)
 
     def testLog(self):
-        resp = transcendentFunction (14, 'LOG')
+        resp = op.transcendentFunction (14, 'LOG')
         self.assertEqual(resp[0], 2.6390573296152584)
 
     def testInverseX(self):
-        resp = transcendentFunction (10, '1/X')
+        resp = op.transcendentFunction (10, '1/X')
         self.assertEqual(resp[0], 0.1)
 
 if __name__ == '__main__':
